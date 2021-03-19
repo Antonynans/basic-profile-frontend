1ea3af45f242f0390f5410971701df1d5b885704
antonynans <antonynans@gmail.com>
2021-03-19 23:40:51 +0100

    created the backend folder

---
+++ src/component/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Formik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+export default function Register() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
